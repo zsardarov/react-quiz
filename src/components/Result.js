0354610147632a4665494a625691fa8a484e4bef
zura_sardarov <oblivion5733@gmail.com>
2021-01-01 13:28:11 +0400

    Edit score indicator.

--- src/components/Result.js
+++ src/components/Result.js
@@ -2,13 +2,13 @@ import React from "react";
 import {connect} from "react-redux";
 import {restartQuiz} from "../redux/actions";
 
-const Result = ({quiz, restartQuiz}) => {
+const Result = ({quiz, questions, restartQuiz}) => {
     return (
         <>
             <h1 className="text-center">Results</h1>
             <div className="row">
                 <div className="offset-md-2 col-md-8 text-center">
-                    <p>Score: {quiz.score}</p>
+                    <p>Score: {quiz.score} / {questions.length}</p>
                     <p>Time: {quiz.endTime.diff(quiz.startTime, 'minutes', true).toFixed(2)} minutes</p>
                     <button className="btn btn-success" onClick={restartQuiz}>Restart Quiz</button>
                 </div>
@@ -21,6 +21,7 @@ const Result = ({quiz, restartQuiz}) => {
 const mapStateToProps  = (state) => {
     return {
         quiz: state.quiz,
+        questions: state.menu.questions
     }
 };
 
